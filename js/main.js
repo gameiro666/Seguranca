b380b2162a76675241c9ab9e76877ec71a090179
gameiro666 <61329564+gameiro666@users.noreply.github.com>
2021-05-17 22:23:39 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+async function request(url) {
+  const response = await fetch(url)
+  const json = await response.json()
+  return json
+}
+
+
+const btnForm = document.getElementById('btnForm')
+btnForm.addEventListener('click', (e) => {
+  e.preventDefault()
+  const inputEmail = document.getElementById('email').value
+  const url = request(`https://helioheroku.herokuapp.com/2factors/${inputEmail}`)
+  console.log(url)
+})
+
+
